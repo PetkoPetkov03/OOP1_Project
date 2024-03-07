60af530584d12111ed8c850194a5aaa7b8198ac0
PetkoPetkov03 <komandir1612@gmail.com>
2024-03-07 15:24:39 +0200

    Create book structure && initialize Library

--- src/bg/tu/varna/SIT/s22621616/a2/Application.java
+++ src/bg/tu/varna/SIT/s22621616/a2/Application.java
@@ -1,7 +1,17 @@
 package bg.tu.varna.SIT.s22621616.a2;
 
+import java.util.HashMap;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashMap<String, Books> booksHashMap = new HashMap<>();
+
+        Books book = new Books("t", "tete", Genre.ADVENTURE, "sdgs", new Keywords[]{Keywords.TEMPLATE},
+                2003, 5.0F, "sdghsdg");
+
+        booksHashMap.put(book.id, book);
+        Library lib = new Library(booksHashMap);
+
+        lib.printBooks();
     }
 }
